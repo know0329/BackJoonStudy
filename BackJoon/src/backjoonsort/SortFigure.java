f1e6d91710037abb710f7d4ab17fe4062cb58237
know0329 <49915643+know0329@users.noreply.github.com>
2022-02-24 20:50:38 +0900

    2022.02.24 백준 2750번

---
+++ BackJoon/src/backjoonsort/SortFigure.java
@@ -0,0 +1,38 @@
+package backjoonsort;
+
+
+import java.util.Scanner;
+
+//น้มุ 2750น๘
+	
+public class SortFigure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner scn = new Scanner(System.in);
+		
+		int arraySize = scn.nextInt();
+		int figureList[] = new int[arraySize];
+		for(int i = 0; i < arraySize; i++) {
+			figureList[i] = scn.nextInt();
+		}
+		
+		for(int i = 0; i < arraySize; i++) {
+			for(int j = i+1; j < arraySize; j++) {
+				if(figureList[i] > figureList[j]) {
+					int temp = figureList[i];
+					figureList[i] = figureList[j];
+					figureList[j] = temp;
+				}
+				
+			}
+		}
+		
+		for(int i = 0 ; i < arraySize; i++) {
+			System.out.println(figureList[i]);
+		}
+	}
+
+}
